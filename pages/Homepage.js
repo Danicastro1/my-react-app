b5f8b578ac29ec6056fa54f7d22fe2fb1eeaac0b
Daniela Castro <20danicastro15@gmail.com>
2023-09-24 14:49:15 -0700

    added navigation bar

---
+++ pages/Homepage.js
@@ -0,0 +1,14 @@
+import {Container} from 'reactstrap'
+import {Header} from '../src/components/Header'
+
+
+const HomePage = () => {
+    return (
+        <Container>
+            <Header/>
+        </Container>
+
+    )
+};
+
+export default HomePage;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,75 @@
 // import {useState} from 'react';
 
+// import {Navbar} from 'reactstrap';
+// import {NavbarBrand} from 'reactstrap';
+// import Screenshot from '../img/Screenshot.png'
+
+
+// const Header = () => {
+//     // const [menuOpen, setMenuOpen] = useState(false);
+
+//     return (<Navbar sticky='top' expand='md'>
+//          <NavbarBrand className='ms-5' href='/'>
+//              <img src={Screenshot} alt='logo' className='float-start'/>
+             
+//          </NavbarBrand>
+//         </Navbar>
+//         )
+//     }
+         
+
+
+// export default Header;
+
+import {useState} from 'react';
+import {Collapse, NavbarToggler, Nav, NavItem} from 'reactstrap';
+import {NavLink} from 'react-router-dom';
 import {Navbar} from 'reactstrap';
 import {NavbarBrand} from 'reactstrap';
 import Screenshot from '../img/Screenshot.png'
 
 
 const Header = () => {
-    // const [menuOpen, setMenuOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
-    return (<Navbar sticky='top' expand='md'>
+    return (
+    <Navbar dark color='primary' sticky='top' expand='md'>
          <NavbarBrand className='ms-5' href='/'>
-             <img src={Screenshot} alt='logo' className='float-start'/>
-             
+             <img src={Screenshot} alt='Deep Connections logo' className='float-start'/>
+             <h1 className='mt-1'>Deep Connections</h1>
          </NavbarBrand>
-        </Navbar>
-        )
-    }
-         
 
+         <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='ms-auto' navbar>
+                </Nav>
+
+            <Nav className='ms-auto' navbar>
+                <NavItem>
+                    <NavLink className='nav-link' to ='/home'>
+                        <i className='fa fa-home fa-lg' /> Home
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className='nav-link' to ='/'>
+                        <i className='fa fa-info fa-lg' /> Appointments
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className='nav-link' to ='/about'>
+                        <i className='fa fa-info fa-lg' /> About
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className='nav-link' to ='/contact'>
+                        <i className='fa fa-address-card fa-lg' /> Contact
+                    </NavLink>
+                </NavItem>
+            </Nav>
+            
+         </Collapse>
+ </Navbar>
+ )
+}
 
 export default Header;
